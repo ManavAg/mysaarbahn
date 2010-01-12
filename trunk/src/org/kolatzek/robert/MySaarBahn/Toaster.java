b29e5933ca72855860ffc5f5cb235d2f9b75aa9a
UmbertoKo <UmbertoKo@925a0f94-ffb7-11de-95d2-51bf8322dccd>
2010-01-12 20:50:41 +0000

    initial version

---
+++ trunk/src/org/kolatzek/robert/MySaarBahn/Toaster.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package org.kolatzek.robert.MySaarBahn;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * @author neo
+ *
+ */
+public class Toaster implements Runnable 
+{
+	Context context;
+	String text = null;
+	int duration;
+	int resource = 0;
+	public Toaster(Context mycontext, String mytext, int myduration) {
+		context = mycontext;
+		text = mytext;
+		duration = myduration;
+	}
+	
+	public Toaster(Context mycontext, int myresource, int myduration) {
+		context = mycontext;
+		resource = myresource;
+		duration = myduration;
+	}
+
+	public void run() {
+		
+		if(text != null)
+		{
+			Toast.makeText(context, text, duration).show();
+		}
+		else
+		{
+			Toast.makeText(context, resource, duration).show();
+		}
+	}
+}
