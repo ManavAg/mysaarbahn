e346fab51e3a601b847f9f00e3f10976a7871be6
UmbertoKo <UmbertoKo@925a0f94-ffb7-11de-95d2-51bf8322dccd>
2010-03-06 15:38:10 +0000

    Create branch for version 2

---
+++ branches/OpenStreetmap/src/org/kolatzek/robert/MySaarBahn/Station.java
@@ -0,0 +1,41 @@
+/**
+ * Station and translation to Geopoint
+ * @author Umberto
+ * Under GPLv3 license
+ */
+package org.kolatzek.robert.MySaarBahn;
+
+import com.google.android.maps.GeoPoint;
+
+import android.location.Location;
+
+public class Station {
+	public String name;
+	public double latitude;
+	public double logitude;
+	public String shortname;
+	public double distance;
+	public long time = 0;
+	public int position = 0;
+	public Station(String n, double d, double e, String shortname, int position)
+	{
+		this.name = n;
+		this.latitude = d;
+		this.logitude = e;
+		this.shortname = shortname;
+		this.position = position;
+	}
+	
+	public Location getLocation()
+	{
+		Location l = new Location("gps");
+		l.setLongitude(this.logitude);
+		l.setLatitude(this.latitude);
+		return l;
+	}
+	
+	public GeoPoint getGeopoint()
+	{
+		return new GeoPoint((int)(this.latitude*1E6), (int)(this.logitude*1E6));
+	}
+}
